0f406ba7940f860f5765424dc3c2881cb84744c1
ssbrandt <sierrasbrandt@gmail.com>
2024-03-27 08:53:33 -0400

    update css for savednewsheader

--- news-explorer-frontend/src/components/SavedNewsHeader/SavedNewsHeader.js
+++ news-explorer-frontend/src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -4,9 +4,12 @@ import React from "react";
 function SavedNewsHeader() {
   return (
     <section className="saved-news">
-      <p>Saved articles</p>
-      <h1>Elise, you have 5 saved articles</h1>
-      <p>By keywords: Word, Word, Word</p>
+      <p className="saved-news__subheader">Saved articles</p>
+      <h1 className="saved-news__header">Elise, you have 5 saved articles</h1>
+      <div className="saved-news__keywords">
+        <p className="keywords__intro">By keywords: </p>
+        <p className="keywords__items">Word, Word, Word</p>
+      </div>
     </section>
   );
 }
