aad38528a48f6649c7a918fc875f18400f6d2bd8
ssbrandt <sierrasbrandt@gmail.com>
2024-03-28 16:28:50 -0400

    add images to footer

--- news-explorer-frontend/src/components/Footer/Footer.js
+++ news-explorer-frontend/src/components/Footer/Footer.js
@@ -1,15 +1,23 @@
 import "./Footer.css";
 import React from "react";
+import github from "../../images/github.svg";
+import facebook from "../../images/facebook.svg";
 
-function Footer({}) {
+function Footer() {
   return (
     <footer className="footer">
       <h2 className="footer__title">2024 Supersite, Powered by News API</h2>
       <ul className="footer__items">
         <li className="footer__item">Home</li>
         <li className="footer_item">Practicum</li>
-        <li className="footer_item">Github</li>
-        <li className="footer_item">Facebook</li>
+        <div className="footer__images">
+          <li className="footer_item">
+            <img src={github} alt="github" />
+          </li>
+          <li className="footer_item">
+            <img src={facebook} alt="facebook" />
+          </li>
+        </div>
       </ul>
     </footer>
   );
