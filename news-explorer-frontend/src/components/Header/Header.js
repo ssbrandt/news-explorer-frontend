f3888dcc4622abc61e229fea010d73dc671a13c7
ssbrandt <sierrasbrandt@gmail.com>
2024-04-07 15:50:42 -0400

    tweak tablet header layout

--- news-explorer-frontend/src/components/Header/Header.js
+++ news-explorer-frontend/src/components/Header/Header.js
@@ -5,9 +5,6 @@ import Navigation from "../Navigation/Navigation";
 function Header({ onSignInModal, loggedIn, darkMode }) {
   return (
     <header className={darkMode ? "header-dark" : "header"}>
-      {/* <h2 className={darkMode ? "header__title-dark" : "header__title"}>
-        NewsExplorer
-      </h2> */}
       <Navigation
         onSignInModal={onSignInModal}
         loggedIn={loggedIn}
